dfdf6f87544ba286b52e22668e359b5f96996f47
Glauber <yesshua100@gmail.com>
2024-07-04 22:07:34 -0300

    feet: Criada Estrutura Base do Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 //import Batatinha from "./components/Header"
-
+import "./styles/index.css"
 import { LadingPage } from "./pages/LadingPage"
 
 
 const App = () => {
   
-
-
   return (
     <> 
       <LadingPage/>
